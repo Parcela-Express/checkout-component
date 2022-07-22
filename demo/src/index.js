5065be2d84d6053cb70aaa1df417a48636b0bebf
Vinicius Miranda <vmirandacintra@gmail.com>
2022-07-22 10:41:14 -0300

    fix demo test url

--- demo/src/index.js
+++ demo/src/index.js
@@ -50,7 +50,7 @@ const Demo = () => {
           >
             <Checkout
               environment={"TEST"}
-              apiUrl={`http://localhost:9010`}
+              apiUrl={`https://sandbox.parcelaexpress.com.br`}
               customerData={{
                 amount_cents: 20000,
                 description: "Venda Teste",
