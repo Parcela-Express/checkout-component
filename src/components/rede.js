28eb11050ce247dd747f4f504922b1024aab634c
Alexandre Queiroz <alexandrequeiroz.dev@gmail.com>
2024-03-01 15:01:51 -0300

    feat: add card patterns on rede component

--- src/components/rede.js
+++ src/components/rede.js
@@ -15,6 +15,11 @@ import {
 import { FaLock } from 'react-icons/fa';
 import CvcSvg from '../icons/cvc';
 import NoCardSvg from '../icons/nocard';
+import VisaSvg from '../icons/visa';
+import McSvg from '../icons/mc';
+import EloSvg from '../icons/elo';
+import AmexSvg from '../icons/amex';
+import HiperSvg from '../icons/hiper';
 
 const Rede = (props) => {
   const [cardNumber, setCardNumber] = useState('');
@@ -25,12 +30,34 @@ const Rede = (props) => {
   const [expiryValid, setExpiryValid] = useState(false);
   const [cvcValid, setCvcValid] = useState(false);
   const [cardHolderNameValid, setCardHolderNameValid] = useState(false);
+  const [cardIcon, setCardIcon] = useState(<NoCardSvg />);
+
+  const detectCardType = (number) => {
+    const cleanedNumber = number.replace(/\s/g, '');
+    const cardTypes = [
+      { type: 'visa', pattern: /^4\d{5}/ },
+      { type: 'mc', pattern: /^(5[1-5]|222[1-9]|22[3-9]|2[3-6]|27[01]|2720)[0-9]/ },
+      {
+        type: 'elo',
+        pattern:
+          /^(4011|431274|438935|451416|457393|4576|457631|457632|504175|627780|636297|636368|636369|(6503[1-3])|(6500(3[5-9]|4[0-9]|5[0-1]))|(6504(0[5-9]|1[0-9]|2[0-9]|3[0-9]))|(650(48[5-9]|49[0-9]|50[0-9]|51[1-9]|52[0-9]|53[0-7]))|(6505(4[0-9]|5[0-9]|6[0-9]|7[0-9]|8[0-9]|9[0-8]))|(6507(0[0-9]|1[0-8]))|(6507(2[0-7]))|(650(90[1-9]|91[0-9]|920))|(6516(5[2-9]|6[0-9]|7[0-9]))|(6550(0[0-9]|1[1-9]))|(6550(2[1-9]|3[0-9]|4[0-9]|5[0-8]))|(506(699|77[0-8]|7[1-6][0-9))|(509([0-9][0-9][0-9])))/
+      },
+      { type: 'amex', pattern: /^3[47]\d{13}$/ },
+      { type: 'hipercard', pattern: /^(606282\d{10}(\d{3})?)|(3841\d{12})|(637\d{13})$/ },
+      { type: 'maestro', pattern: /^(?:5[0678]\d\d|6304|6390|67\d\d)\d{8,15}$/ }
+    ];
+    for (const { type, pattern } of cardTypes) {
+      if (pattern.test(cleanedNumber)) {
+        return type;
+      }
+    }
+    return 'default';
+  };
 
   const validateCardNumber = (number) => {
     const cardNumber = number.replace(/\s/g, '');
     let sum = 0;
     let doubleUp = false;
-
     for (let i = cardNumber.length - 1; i >= 0; i--) {
       let digit = parseInt(cardNumber.charAt(i), 10);
       if (doubleUp) {
@@ -42,7 +69,6 @@ const Rede = (props) => {
       sum += digit;
       doubleUp = !doubleUp;
     }
-
     return sum % 10 === 0;
   };
 
@@ -55,6 +81,18 @@ const Rede = (props) => {
 
   const handleCardNumberChange = (e) => {
     const input = e.target.value;
+    const cardType = detectCardType(input);
+    const cardIcons = {
+      visa: <VisaSvg />,
+      mc: <McSvg />,
+      elo: <EloSvg />,
+      amex: <AmexSvg />,
+      hipercard: <HiperSvg />,
+      maestro: <McSvg />,
+      default: <NoCardSvg />
+    };
+    setCardIcon(cardIcons[cardType]);
+
     if (input.length <= 19) {
       const formattedInput = formatCardNumber(input);
       setCardNumber(formattedInput);
@@ -72,8 +110,8 @@ const Rede = (props) => {
       .replace(/\D/g, '')
       .replace(/^(\d{2})(\d{0,2})/, '$1/$2')
       .replace(/^(\d{2})\/(\d{2}).*/, '$1/$2');
-    setExpiry(formattedInput);
     const isValidFormat = /^(0[1-9]|1[0-2])\/\d{2}$/.test(formattedInput);
+
     if (isValidFormat) {
       const [month, year] = formattedInput.split('/');
       const currentYear = new Date().getFullYear().toString().slice(-2);
@@ -89,6 +127,8 @@ const Rede = (props) => {
     } else {
       setExpiryValid(false);
     }
+
+    setExpiry(formattedInput);
   };
 
   const handleCvcChange = (e) => {
@@ -129,8 +169,8 @@ const Rede = (props) => {
               color="#90a2bd"
               borderColor={cardNumberValid && cardNumber?.length === 19 ? 'green.500' : ''}
             />
-            <InputRightElement pointerEvents="none">
-              <NoCardSvg />
+            <InputRightElement w={'27px'} mr={2} pointerEvents="none">
+              {cardIcon}
             </InputRightElement>
           </InputGroup>
           <FormErrorMessage fontSize="12px" fontWeight="400">
